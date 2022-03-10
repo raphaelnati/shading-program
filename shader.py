b07afa454687f8a071be6ffe33825181067def2f
Raphael Nation <raphael.nation@ontariotechu.net>
2022-03-09 21:06:35 -0500

    added comments to file

--- shader.py
+++ shader.py
@@ -18,12 +18,19 @@ inputimg = cv.imread(sys.argv[1])
 inputhsv = cv.cvtColor(inputimg, cv.COLOR_BGR2HSV)
 
 def hueToShadeVec(imghues):
+    #input
+    #slice of image hues
+    #move hue towards purple for darker looking colour
     diff, direct = closerEndVec(imghues)
+
+    #minimize distant by factor and then apply direction 
     funco = (np.sqrt(diff) * 2 * direct)
     newhue = levelerVec(np.copy(imghues) + funco)
     return newhue
 
 def levelerVec(img):
+    #input regular adjusted hues from hueToShadeVec and output values between 0 - 180
+    #neccesary to have hue values wrap around range of 180
     huem = np.copy(img)
     undermask = huem < 0
     overmask = huem > 180
@@ -32,6 +39,10 @@ def levelerVec(img):
     return huem
 
 def closerEndVec(img):
+    #input
+    #slice of image hues
+    #output vector "diff", amount to move by along hue value
+    #vector "dirs" indicating which direction the hues should go
     huem = np.copy(img)
     diff = np.copy(img)
     dirs = np.copy(img)
@@ -50,6 +61,10 @@ def closerEndVec(img):
     return diff, dirs
 
 def newVSVec(img2,scale):
+    #input
+    #slice of image saturation and value
+    #scale to move across vector field (around 1-2 usually)
+    #output new value and saturation based on vector field equation
     img = np.copy(img2)
     s = img[:,:,0]
     v = img[:,:,1]
@@ -57,11 +72,14 @@ def newVSVec(img2,scale):
     newv = v - (20 * scale)
     img[:,:,0] = news
     img[:,:,1] = newv
+
+    #constrain values between 0 and 255
     img = np.clip(img,a_min=0,a_max=255)
     return img
 
 t0 = time.time()
 img0hsv = np.copy(inputhsv)
+#convert image from uints to ints
 img0hsv = np.intc(img0hsv)
 masker = img0hsv.sum(axis=(2)) == 0
 img0hsv[:,:,0] = hueToShadeVec(img0hsv[:,:,0])
